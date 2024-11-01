42fd0326f3e89f26cfeece9f43ab5f6ab3e06c7c
Kimikhwan <moonfgg@naver.com>
2024-11-02 03:20:32 +0900

    로비매니저, 로비UI컨트롤러, 로비, SettingUI, 확인창UI

---
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SettingsUI : BaseUI
+{
+    public TextMeshProUGUI gameVersionTxt;
+
+    public GameObject soundOnToggle;
+    public GameObject soundOffToggle;
+
+    public override void SetInfo(BaseUIData uiData)
+    {
+        base.SetInfo(uiData);
+
+        SetGameVersion();
+    }
+
+    private void SetGameVersion()
+    {
+        gameVersionTxt.text = $"Version:{Application.version}";
+    }
+
+    private void SetSoundSetting(bool sound)
+    {
+        soundOnToggle.SetActive(sound);
+        soundOffToggle.SetActive(!sound);
+    }
+
+    public void OnClickSoundOnToggle()
+    {
+        AudioManager.Instance.PlaySFX(SFX.ui_button_click);
+    }
+
+    public void OnClickSoundOffToggle()
+    {
+        AudioManager.Instance.PlaySFX(SFX.ui_button_click);
+    }
+
+}
