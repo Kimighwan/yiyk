58b3b2bf2e75dbf5a657d9e802785781b312be6e
Kimikhwan <moonfgg@naver.com>
2024-11-02 02:41:50 +0900

    로고, AudioManager, SceneLoader, Title, Singleton, UI

---
+++ Assets/Scripts/Common/SceneLoader.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneType
+{
+    Title,
+    Lobby,
+    InGame,
+}
+
+public class SceneLoader : SingletonBehaviour<SceneLoader>
+{
+    public void LoadScene(SceneType sceneType)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneType.ToString());
+    }
+
+    public void ReloadScene()
+    {
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public AsyncOperation LoadSceneAsync(SceneType sceneType)
+    {
+        Time.timeScale = 1f;
+        return SceneManager.LoadSceneAsync(sceneType.ToString());
+    }
+}
