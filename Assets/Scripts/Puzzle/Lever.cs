50e2ab739d52d5eaa74ce1faa4187c6824e645b3
Kimikhwan <moonfgg@naver.com>
2024-11-02 22:35:26 +0900

    쳅터5 두 번째 벽

--- Assets/Scripts/Puzzle/Lever.cs
+++ Assets/Scripts/Puzzle/Lever.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 
 public class Lever : MonoBehaviour, IPuzzle
 {
-    public GameObject door;
+    //public GameObject door;
+    public GameObject door1;
+    //public GameObject door2;
+
     public GameObject lever;
 
-    private Animation doorAnim;
+    //private Animator doorAnim;
+    private Animator doorAnim1;
+    //private Animator doorAnim2;
+
     private Animator leverOn;
 
     private bool alreadyOpen = false;
@@ -17,7 +23,10 @@ public class Lever : MonoBehaviour, IPuzzle
     
     private void Awake()
     {
-        doorAnim = door.GetComponent<Animation>();
+        //doorAnim = door.GetComponent<Animator>();
+        doorAnim1 = door1.GetComponent<Animator>();
+        //doorAnim2 = door2.GetComponent <Animator>();
+
         leverOn = lever.GetComponent<Animator>();
         //leverOn.StopPlayback();
     }
@@ -27,7 +36,9 @@ public class Lever : MonoBehaviour, IPuzzle
         if (alreadyOpen) // 열려있으면 그냥 종료
             return;
 
-        doorAnim.Play();
+        //doorAnim.Play();
+        doorAnim1.SetBool("isWall2Down", door1.transform.position.y == 16);
+        //doorAnim2.Play();
 
         alreadyOpen = true; // 열려있다는 것을 체크
     }
