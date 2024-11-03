b237003fb8f64bec6bc15de7e2df6a88b4ceefe8
Kimikhwan <moonfgg@naver.com>
2024-11-03 12:35:34 +0900

    발판, 레버 수정

--- Assets/Scripts/Puzzle/Lever1.cs
+++ Assets/Scripts/Puzzle/Lever1.cs
@@ -4,22 +4,28 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Lever : MonoBehaviour, IPuzzle
+public class Lever1 : MonoBehaviour, IPuzzle
 {
     public GameObject door;
+    public GameObject door1;
+    public GameObject door2;
 
     public GameObject lever;
 
-    private Animation doorAnim;
+    private Animator doorAnim;
+    private Animator doorAnim1;
+    private Animator doorAnim2;
 
     private Animator leverOn;
 
     private bool alreadyOpen = false;
     //Animator leverOn;
-    
+
     private void Awake()
     {
-        doorAnim = door.GetComponent<Animation>();
+        doorAnim = door.GetComponent<Animator>();
+        doorAnim1 = door1.GetComponent<Animator>();
+        doorAnim2 = door2.GetComponent<Animator>();
 
         leverOn = lever.GetComponent<Animator>();
         //leverOn.StopPlayback();
@@ -30,7 +36,9 @@ public class Lever : MonoBehaviour, IPuzzle
         if (alreadyOpen) // 열려있으면 그냥 종료
             return;
 
-        doorAnim.Play();
+        doorAnim.SetBool("isWall1Up", door.transform.localPosition.y == 0);
+        doorAnim1.SetBool("isWall2Down", door1.transform.localPosition.y == 16);
+        doorAnim2.SetBool("isWall3Up", door2.transform.localPosition.y == 0);
 
         alreadyOpen = true; // 열려있다는 것을 체크
     }
