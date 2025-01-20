3c79d43e22911786741301987a53e374c6daaa87
Kimikhwan <moonfgg@naver.com>
2025-01-21 01:15:37 +0900

    스테이지 DB 구현, 클리어시 로비로 나감, 인게임 소리 조절 토글 선택 안 되는 버그 수정

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class LobbyManager : MonoBehaviour
 {
+    private void Awake()
+    {
+        SetStage();
+    }
+
+    public GameObject[] Stage;
     public void OnClickStageBtn(int stage) // 1 ~ 10
     {
         SceneLoader.Instance.LoadScene((SceneType)(stage + 3));
         AudioManager.Instance.PlayBGM(BGM.IngameBGM);
+    }
+
+    private void SetStage()
+    {
+        if(PlayerPrefs.GetInt("Stage") != 0)
+        {
+            for (int index = 1; index <= PlayerPrefs.GetInt("Stage"); index++)
+            {
+                var clearUI = Instantiate(Resources.Load<GameObject>("UI/Clear"));
+                clearUI.transform.parent = Stage[index - 1].transform;
+                clearUI.GetComponent<RectTransform>().anchoredPosition = new Vector2(81, -71);
+            }
+        }
+
+        for(int index = PlayerPrefs.GetInt("Stage") + 2; index <= 10; index++)
+        {
+            var lockUI = Instantiate(Resources.Load<GameObject>("UI/Lock"));
+            lockUI.transform.parent = Stage[index - 1].transform;
+            lockUI.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        }
     }
 }
